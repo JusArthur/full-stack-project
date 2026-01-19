586169f8f0210fdf3f9a6f96b8db620e3fe30f1d
Fan Luo <fluotechdev@gmail.com>
2026-01-19 00:24:14 -0600

    update Footer component to display group members

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,10 @@
 const Footer = () => {
   return (
     <footer className="w-full text-center py-4 bg-[#C8102E] text-white">
-      <p className="m-0 text-sm">@CopyWrong 2026 - Team Hortons</p>
+      <p className="m-0 text-l">@CopyWrong 2026 - Team Hortons</p>
+      <div className="mt-2 text-s">
+        Group Members: Fan Luo, Yunfei Wu, Justin Xia
+      </div>
     </footer>
   );
 };

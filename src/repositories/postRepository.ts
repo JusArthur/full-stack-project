1d618f4add01c1c0d4bdb63705b93ff7c5dd2780
JusArthur <arthurkingjustin@gmail.com>
2026-02-24 14:27:44 -0600

    feat: add post repository and mock data for community posts

---
+++ src/repositories/postRepository.ts
@@ -0,0 +1,18 @@
+import { portTestData } from "../testData/postTestData";
+import type { CommunityPost } from "../components/home/types/communitypost";
+
+// mock the database
+let posts = [...portTestData];
+
+export const postRepository = {
+    async getAll(): Promise<CommunityPost[]> {
+        return new Promise((resolve) => setTimeout(() => resolve(posts), 500)); // Simulate async delay
+    },
+    async add(post: CommunityPost): Promise<CommunityPost> {
+        posts.unshift(post);
+        return post;
+    },
+    async delete(id: string): Promise<void> {
+        posts = posts.filter(p => p.id !== id);
+    }
+}
